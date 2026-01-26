9d596e533d2a12e22cd14b4c3c6eeff88144e164
ivona21 <ivonamalekovic@hotmail.com>
2026-01-26 15:53:25 +0100

    zod incorporated into items form

--- src/features/items/forms/ItemForm.tsx
+++ src/features/items/forms/ItemForm.tsx
@@ -3,12 +3,17 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { ItemFormValues, itemSchema } from '@/features/items/forms/item.schema.ts';
 import { Input } from '@/components/ui/Input.tsx';
 import { Button } from '@/components/ui/Button.tsx';
-import { Form, FormControl, FormField, FormMessage } from '@/components/form/Form.tsx';
-import { FormLabel } from '@/components/ui/Form.tsx';
+import { Form, FormControl, FormField, FormMessage, FormLabel } from '@/components/form/Form.tsx';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export default function ItemForm() {
   const { mutate: createItem, isPending } = useCreateItem();
-  const methods = useForm<ItemFormValues>();
+  const methods = useForm<ItemFormValues>({
+    resolver: zodResolver(itemSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
 
   const onSubmit: SubmitHandler<ItemFormValues> = (data) => {
     createItem(data.name);
