c44d938701a0c0a9f9f93db478e44b5aed7079a8
ivona21 <ivonamalekovic@hotmail.com>
2026-02-15 12:39:57 +0100

    heart changing colors - just for fun

--- src/features/svgs/HeartIcon.tsx
+++ src/features/svgs/HeartIcon.tsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
+
 export default function HeartIcon() {
+  const [heartColor, setHeartColor] = useState('red');
+
+  const generateRandomHex = () => {
+    const hex = Math.floor(Math.random() * 0xffffff).toString(16);
+    return `#${hex.padStart(6, '0')}`;
+  };
+
+  const changeColor = () => {
+    let newColor = heartColor;
+
+    // keep generating until it’s different
+    while (newColor === heartColor) {
+      newColor = generateRandomHex();
+    }
+
+    setHeartColor(newColor);
+  };
+
   return (
-    <svg height="100" width="100" viewBox="0 0 100 100">
+    <svg
+      height="100"
+      width="100"
+      viewBox="0 0 100 100"
+      style={{ cursor: 'pointer' }}
+      onClick={changeColor}
+    >
       <path
         d="M 25 25 L 50 50 L 75 25"
         strokeWidth="50"
-        stroke="red"
+        stroke={heartColor}
         strokeLinecap="round"
         fill="none"
       />
--- src/pages/learning-svg.tsx
+++ src/pages/learning-svg.tsx
@@ -3,7 +3,6 @@ import Drawings from '@/features/svgs/Drawings.tsx';
 export default function LearningSvgPage() {
   return (
     <div>
-      My space for learning svg
       <Drawings />
     </div>
   );
