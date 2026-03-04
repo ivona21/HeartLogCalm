1c4ff31031bae22498ed88ff042f2513a5eae70a
ivona21 <ivonamalekovic@hotmail.com>
2026-03-04 17:59:03 +0100

    defs and use in lamp arms

--- src/features/svgs/Lamp.tsx
+++ src/features/svgs/Lamp.tsx
@@ -1,13 +1,22 @@
 export default function Lamp() {
   return (
     <svg height="200" width="200" viewBox="0 0 200 200">
+      <defs>
+        <g id="lamp-arm">
+          <circle r="5" />
+          <path stroke="black" strokeWidth="7" strokeLinecap="round" d="M 0 0 L 0 -70" />
+        </g>
+        <g id="lamp-arm-short">
+          <circle r="5" />
+          <path stroke="black" strokeWidth="7" strokeLinecap="round" d="M 0 0 L 0 -30" />
+        </g>
+      </defs>
       <g transform="translate(70, 170) rotate(-25)">
-        <circle r="5" />
-        <path stroke="black" strokeWidth="7" strokeLinecap="round" d="M 0 0 L 0 -70" />
-        <g transform="translate(0, -70)">
-          <g transform="rotate(50)">
-            <circle r="5" />
-            <path stroke="black" strokeWidth="7" strokeLinecap="round" d="M 0 0 L 0 -70" />
+        <use xlinkHref="#lamp-arm" />
+        <g transform="translate(0, -70) rotate(50)">
+          <use xlinkHref="#lamp-arm" />
+          <g transform="translate(0, -70) rotate(50)">
+            <use xlinkHref="#lamp-arm-short" />
           </g>
         </g>
       </g>
