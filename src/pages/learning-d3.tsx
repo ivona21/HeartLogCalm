ca6af574091b9198907bca1dce2f52e043031a9e
ivona21 <ivonamalekovic@hotmail.com>
2026-03-14 10:20:25 +0100

    all pages for learning and practicing removed from the project

--- src/pages/learning-d3.tsx
+++
@@ -1,3 +0,0 @@
-export default function LearningD3Page() {
-  return <div>d3 page</div>;
-}
--- src/pages/learning-svg.tsx
+++
@@ -1,9 +0,0 @@
-import Drawings from '@/features/svgs/Drawings.tsx';
-
-export default function LearningSvgPage() {
-  return (
-    <div>
-      <Drawings />
-    </div>
-  );
-}
