476f3d1c8146faab839a32bf40fc45e9c9e44cfc
ivona21 <ivonamalekovic@hotmail.com>
2026-03-17 08:44:58 +0000

    emotion wheel spacings

--- src/pages/emotion-wheel.tsx
+++ src/pages/emotion-wheel.tsx
@@ -2,8 +2,8 @@ import { Wheel } from '@/features/emotion-wheel/components/Wheel.tsx';
 
 export default function EmotionWheelPage() {
   return (
-    <div className="-mt-[48px] -mb-[48px] -mx-4 md:mt-0 md:mb-0 md:mx-0 md:flex md:flex-col md:items-center md:gap-6">
-      <div className="w-full h-[100dvh] -translate-y-[20px] md:translate-y-0 md:h-auto md:min-h-[340px] md:max-w-[820px]">
+    <div className="-mt-[48px] -mx-4 md:mt-0 md:mb-0 md:mx-0 md:flex md:flex-col md:items-center md:gap-6">
+      <div className="w-full h-[100dvh] md:translate-y-0 md:h-auto md:min-h-[340px] md:max-w-[820px]">
         <Wheel />
       </div>
     </div>
