4f9437ed277500d951c4fc4e58ebf7f24b9469fd
IvonaMS <ivonamalekovic@hotmail.com>
2026-08-26 10:35:41 +0200

    password policy extracted

--- src/features/auth/forms/RegisterForm/schema.ts
+++ src/features/auth/forms/RegisterForm/schema.ts
@@ -1,16 +1,10 @@
-// Register schema and type
 import { z } from 'zod';
+import { passwordSchema } from '@/features/auth/forms/password-policy.ts';
 
 export const registerSchema = z
   .object({
     email: z.string().email('Please enter a valid email address'),
-    password: z
-      .string()
-      .min(8, 'Please use some more secure password. Try using 8 or more characters with a mix of letters, numbers and symbols')
-      .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
-      .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
-      .regex(/[0-9]/, 'Password must contain at least one number')
-      .regex(/[^A-Za-z0-9]/, 'Password must contain at least one special character'),
+    password: passwordSchema,
     confirmPassword: z.string().min(1, 'Please confirm your password'),
   })
   .refine((data) => data.password === data.confirmPassword, {
